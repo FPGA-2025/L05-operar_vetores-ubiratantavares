0666747ba739748c00686feb8663e894386c3da6
Ubiratan Tavares <ust1973@gmail.com>
2025-04-04 19:45:07 -0300

    implementando o arquivo operar_vetores.v

--- operar_vetores.v
+++ operar_vetores.v
@@ -5,5 +5,13 @@ module operar_vetores(
     output saida_or_logico,
     output [5:0] saida_not
 );
-	//insira seu código aqui
+	// OR bit a bit
+    assign saida_or_bit_a_bit = a | b;
+
+    // OR logico (retorna 1 se qualquer uma das entradas for diferente de 0)
+    assign saida_or_logico = (a != 3'b000) || (b != 3'b000);
+
+    // NOT bit a bit da concatenação de b com a
+    assign saida_not = ~{b, a};    
+
 endmodule
